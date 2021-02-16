915040719ac0aa75e65c5521f7e21311d3be210f
Alexey <alexxx1809@gmail.com>
2021-02-16 16:26:37 +0500

    Новый проект, изучаем REST API, Springframework 5, Spring Boot 2 + Swagger2...

---
+++ src/main/java/ru/alex/contollers/MainController.java
@@ -0,0 +1,26 @@
+package ru.alex.contollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    @GetMapping(value = "/hello")
+    public List<String> getProducts() {
+        List<String> products = new ArrayList<>();
+        products.add("Icecream");
+        products.add("Beer");
+        products.add("Water");
+        return products;
+    }
+
+    @PostMapping(value = "hello")
+    public String creatProduct() {
+        return "Product is saved successfully";
+    }
+}
