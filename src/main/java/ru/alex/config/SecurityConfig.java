e151e8d835ea142368c3cf5f0ffe032e6d860ca9
Alexey <alexxx1809@gmail.com>
2021-02-24 16:49:47 +0500

    Исправил ошибки, настроил SecurityConfig

--- src/main/java/ru/alex/config/SecurityConfig.java
+++ src/main/java/ru/alex/config/SecurityConfig.java
@@ -1,19 +1,42 @@
 package ru.alex.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
 
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("123")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
 }
