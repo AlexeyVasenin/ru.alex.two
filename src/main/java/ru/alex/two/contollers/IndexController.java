27b2ec48cba2b3c8e9ca69f5b05d4e0c73a5f085
Alexey <alexxx1809@gmail.com>
2021-08-03 08:05:22 +0500

    Настроил веб

--- src/main/java/ru/alex/two/contollers/IndexController.java
+++ src/main/java/ru/alex/two/contollers/IndexController.java
@@ -1,18 +1,25 @@
 package ru.alex.two.contollers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping
 public class IndexController {
     @GetMapping("/")
-    public String index(Model model, String name) {
-        model.addAttribute("name", name);
+    public String index() {
 
         return "index";
+    }
+
+    @GetMapping("/regis")
+    public String regis() {
+        return "regis";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
     }
 }
